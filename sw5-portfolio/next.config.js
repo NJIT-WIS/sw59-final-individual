cb2eaa093a613cdbaeaa212eae26e83ef12a6067
sw59 <sw59@njit.edu>
2023-08-02 15:22:21 +0800

    fix

--- sw5-portfolio/next.config.js
+++ sw5-portfolio/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     output: 'export',
     eslint: {
         ignoreDuringBuilds: true,
-      },
+      }, 
+    images: { unoptimized: true }
 }
 
 module.exports = nextConfig
